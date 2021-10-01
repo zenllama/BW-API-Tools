7c234a78d3c3ed1618ac92a0fbf38c4c2508c421
Zen Llama <peter.hepburn@zenllama.co.uk>
2021-10-01 09:20:38 +0100

    actually added the script this time :)

---
+++ OpenInvoiceID.ps1
@@ -0,0 +1,7 @@
+﻿$link = ".p2p.basware.com/ap/invoice/details?docId="
+$tenant = "" #put your tenant name here
+
+[void][System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') 
+$docid = [Microsoft.VisualBasic.Interaction]::InputBox('Enter the Invoice ID', 'Basware Invoice Lookup') 
+
+Start-Process "https://$tenant.p2p.basware.com/ap/invoice/details?docId=$docid"
